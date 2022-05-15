63f8cbd02303a1ac93d6c83121c5aabcf07682cc
Sam Yu Gultom <90024633+Samyugultom@users.noreply.github.com>
2022-05-16 03:08:00 +0700

    CRUD ref and out pada Kos Empat Mata
    Kos empat mata adalah sebuah usaha sederhana yang membutuhkan sistem digital untuk melakukan proses pencatatan, informasi dan lain-lain.

---
+++ Default.aspx
@@ -0,0 +1,135 @@
+﻿<%@ Page Title="KosEmpatMata" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="pbo_tm._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <style>
+        #btnInsertion {
+            background-color: rgb(80, 73, 136);
+            border-radius: 20px;
+            padding: 10px;
+            color: white;
+            border: none;
+        }
+        #btnUpdation {
+            background-color: rgb(71 146 119);
+            border-radius: 20px;
+            padding: 10px;
+            color: white;
+            border: none;
+        }
+        #btnSelect{
+            background-color: rgb(78 111 152);
+            border-radius: 20px;
+            padding: 10px;
+            color: white;
+            border: none;
+        }
+        #btnDelete{
+            background-color: rgb(186 96 44);
+            border-radius: 20px;
+            padding: 10px;
+            color: white;
+            border: none;
+        }
+        input[type=text]{
+            border-radius: 10px;
+            padding: 10px;
+        }
+        form{
+            display: flex;
+            flex-direction: row;
+            align-items: start;
+        }
+        table{
+            margin-right: 50px;
+            margin-left: 50px;
+        }
+        body{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+    <table>  
+    <tr>  
+        <td colspan="2">  
+            <h1>Insertion</h1>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td> ID ruangan</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtkosID" runat="server" ForeColor="Purple"></asp:TextBox >  
+        </td>  
+    </tr>  
+    <tr>  
+        <td> nama ruangan</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtkosnameR" runat="server" ForeColor="Purple" ></asp:TextBox>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td> nama penyewa</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtkosnameP" runat="server"  ForeColor="Purple" ></asp:TextBox>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td> fasilitas ruangan</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtkosfasilitas" runat="server"  ForeColor="Purple" ></asp:TextBox>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td colspan="2">  
+            <asp:Button ID="btnInsertion"  ForeColor="Blue" runat="server" Text="Insert" OnClick="btnInsertion_Click" Style="width: 48px" />  
+            <asp:Label ID="lblmsg" runat="server" ForeColor="Red"></asp:Label>  
+            <asp:Button ID="btnUpdation"  ForeColor="Blue" runat="server" Text="Update" OnClick="btnUpdation_Click" />
+        </td>  
+    </tr>  
+</table> 
+    <table>  
+    <tr>  
+        <td>  
+            <h1>Selection</h1>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td>  
+            <asp:Button ID="btnSelect" runat="server" ForeColor="Blue" Text="Select All Data" OnClick="btnSelect_Click" />  
+        </td>  
+    </tr>  
+    <tr>  
+        <td>  
+            <asp:GridView ID="GridView1"  ForeColor="Blue" runat="server"></asp:GridView>  
+        </td>  
+    </tr>  
+</table> 
+
+
+<table>  
+    <tr>  
+        <td colspan="3">  
+            <h1>Deletion</h1>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td> ID Ruangan</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtRuanganID" runat="server"></asp:TextBox>  
+        </td>  
+    </tr>  
+    <tr >  
+        <td colspan="3">  
+            <asp:Button ID="btnDelete"  ForeColor="Red" runat="server" Text="Delete" OnClick="btnDelete_Click" />  
+            <asp:Label ID="lblmessage" runat="server" ForeColor="Red"></asp:Label>  
+        </td>  
+    </tr>  
+</table>  
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using Npgsql;
+
+
+namespace pbo_tm
+{
+    public partial class _Default : Page
+    {
+
+protected void btnInsertion_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            /* Insertion After Validations*/
+            using (NpgsqlConnection connection = new NpgsqlConnection())
+            {
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["connection"].ToString();
+                connection.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "Insert into kos (id_ruangan,nama_ruangan,nama_penyewa,fasilitas_ruangan) values(@ID,@nameR,@nameP,@Fasilitas)";
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new NpgsqlParameter("@ID", Convert.ToInt32(txtkosID.Text)));
+                cmd.Parameters.Add(new NpgsqlParameter("@nameR", txtkosnameR.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@nameP", txtkosnameP.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@Fasilitas", txtkosfasilitas.Text));
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                connection.Close();
+                txtkosfasilitas.Text = "";
+                txtkosnameR.Text = "";
+                txtkosID.Text = "";
+                txtkosnameP.Text = "";
+                lblmsg.Text = "Data Has been Saved";
+            }
+        }
+        catch (Exception ex) {
+                lblmsg.Text = "Data not be saved";
+            }
+    }
+
+        protected void btnSelect_Click(object sender, EventArgs e)
+        {
+            try /* Select After Validations*/
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection())
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["connection"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "Select * from kos";
+                    cmd.CommandType = CommandType.Text;
+                    NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    cmd.Dispose();
+                    connection.Close();
+
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        protected void btnUpdation_Click(object sender, EventArgs e)
+        {
+            try /* Updation After Validations*/
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection())
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["connection"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "update kos set namaruangan=@nameR,nama_penyewa=@nameP,fasilitas_ruangan=@Fasilitas where id_ruangan=@ID";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(new NpgsqlParameter("@ID", Convert.ToInt32(txtkosID.Text)));
+                    cmd.Parameters.Add(new NpgsqlParameter("@nameR", txtkosnameR.Text));
+                    cmd.Parameters.Add(new NpgsqlParameter("@nameP", txtkosnameP.Text));
+                    cmd.Parameters.Add(new NpgsqlParameter("@Fasilitas", txtkosfasilitas.Text));
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    connection.Close();
+                    txtkosfasilitas.Text = "";
+                    txtkosnameR.Text = "";
+                    txtkosID.Text = "";
+                    txtkosnameP.Text = "";
+                    lblmsg.Text = "Data Has been Updated";
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            try /* Deletion After Validations*/
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection())
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["connection"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "Delete from kos where id_ruangan=@ID";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(new NpgsqlParameter("@ID", Convert.ToInt32(txtRuanganID.Text)));
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    connection.Close();
+                    txtkosID.Text = "";
+                    lblmessage.Text = "Data Has been Deleted";
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+    }
+}
